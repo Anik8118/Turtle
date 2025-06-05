2d8ee5c115a938aae67cf586228cb7963740ae01
Anik8118 <anik80846@gmail.com>
2025-06-05 09:40:35 +0600

    third and fourth

---
+++ Fourth.py
@@ -0,0 +1,32 @@
+import turtle
+t = turtle.Turtle()
+
+t.fillcolor('blue')
+t.begin_fill()
+
+t.forward(200)
+t.left(90)
+
+t.forward(200)
+t.left(90)
+
+t.forward(200)
+t.left(90)
+
+t.forward(200)
+t.left(90)
+
+t.penup()
+
+#t.forward(200)
+
+t.goto(90,50)
+
+t.pendown()
+
+t.circle(50)
+
+
+t.end_fill()
+
+turtle.mainloop()
---
+++ Third.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle()
+t.forward(200)
+t.left(135)
+
+t.forward(283)
+t.left(135)
+
+t.forward(200)
+t.left(90)
+
+turtle.mainloop()
