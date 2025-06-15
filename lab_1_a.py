f543e641e4458349ad9408b39b932e78a628b701
Anik8118 <anik80846@gmail.com>
2025-06-15 15:16:51 +0600

    first lab

---
+++ lab_1_a.py
@@ -0,0 +1,19 @@
+import turtle
+t = turtle.Turtle()
+
+t.fillcolor("green")
+t.begin_fill()
+
+t.forward(200)
+
+t.left(135)
+t.forward(283)
+
+t.left(135)
+t.forward(200)
+
+t.end_fill()
+
+turtle.mainloop()
+
+
---
+++ lab_1_b.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Turtle()
+t.forward(200)
+
+t.left(90)
+t.forward(200)
+
+t.left(90)
+t.forward(200)
+
+t.left(90)
+t.forward(200)
+
+'''for x in 4:
+    t.left(90)
+    t.forward(200)'''
+
+turtle.mainloop()
